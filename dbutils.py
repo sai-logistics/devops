92029bb9a34a7f059a33b73f494fdb7c8f3e7c00
rmuniprashanthi <rmuniprashanthi@gmail.com>
2022-08-01 15:20:45 +0530

    second commit on 1/08/22

--- dbutils.py
+++ dbutils.py
@@ -1,4 +1,4 @@
 hello to all hope doing good
 today is goodday
 i am free to practice all questions.
-
+again i am trying to commit the dbutils file on users choice. it very easy to practice all git commands and is interesting too
